e5371d77ed9d275e2bfe2025df60b005841040dd
Baggelis Michos <emichos@ceid.upatras.gr>
2021-06-14 12:47:18 +0300

    Update criminal_detection.sql

--- static/sql/criminal_detection.sql
+++ static/sql/criminal_detection.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 24, 2020 at 08:02 PM
+-- Generation Time: Jun 14, 2021 at 11:43 AM
 -- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.4
+-- PHP Version: 7.2.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,8 +41,7 @@ CREATE TABLE `contact` (
 
 INSERT INTO `contact` (`contact_id`, `first_name`, `last_name`, `email`, `subject`) VALUES
 (1, 'Baggelis', 'Michos', 'emichos@ceid.upatras.gr', 'ds'),
-(2, 'Baggelis', 'Michos', 'emichos@ceid.upatras.gr', 'j'),
-(3, 'sdfa', 'asdf', 'asdf', 'asdf');
+(2, 'Baggelis', 'Michos', 'emichos@ceid.upatras.gr', 'j');
 
 -- --------------------------------------------------------
 
@@ -66,11 +65,9 @@ CREATE TABLE `criminals` (
 --
 -- Dumping data for table `criminals`
 --
-
-INSERT INTO `criminals` (`criminal_id`, `full_name`, `age`, `height`, `weight`, `eye_color`, `biography`, `portrait`, `last_location`, `gender`) VALUES
-(1, 'Jonathan Walters', 25, 1.78, 65, 'Black', 'We have literally no idea who this dude is', 'https://patch.com/img/cdn/users/546525/2013/01/raw/2f182fdc7ad6dbd044e7c2fb8cafca18.jpg', 'RU6 Labw', 'Male'),
-(2, 'Stamatis Goumas', 32, 1.81, 96, 'Brown', 'This one is the shame of HCI Lab', 'https://i.ibb.co/3MjR6Wc/qLM5mEG.png', 'RU6 Lab', 'Male'),
-(3, 'Baggelis Michos', 24, 1.76, 100, 'Brown', 'This dude is 5G \"massonos\" and works somewhere where no human being should ever work.', 'https://i.ibb.co/0cWDRb7/gYyN6Sd.png', 'RU6 Lab', 'Male');
+INSERT INTO `criminals` (`criminal_id`, `full_name`, `age`, `height`, `weight`, `eye_color`, `biography`, `portrait`, `last_location`) VALUES
+(1, 'Jonathan Walters', 55, 1.78, 65, 'Black', 'No past criminal offences!', 'https://patch.com/img/cdn/users/546525/2013/01/raw/2f182fdc7ad6dbd044e7c2fb8cafca18.jpg', 'Memorial Heights, NYC'),
+(2, 'Armando Tzelai', 24, 1.81, 96, 'Brown', 'From Albania. \r\n\r\nNo past criminal offences!', 'https://i.ibb.co/BtyFxvk/Screenshot-2.png', 'New port, Patras');
 
 -- --------------------------------------------------------
 
@@ -90,9 +87,9 @@ CREATE TABLE `users` (
   `mobile_phone` varchar(255) DEFAULT NULL,
   `role` varchar(255) DEFAULT NULL,
   `avatar` varchar(255) DEFAULT NULL,
-  `theme` varchar(255) DEFAULT 'Light Theme',
-  `language` varchar(255) DEFAULT 'English',
-  `fontsize` int(11) DEFAULT 14
+  `theme` varchar(255) NOT NULL,
+  `language` varchar(255) NOT NULL,
+  `fontsize` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -100,11 +97,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `full_name`, `gender`, `biography`, `work_phone`, `mobile_phone`, `role`, `avatar`, `theme`, `language`, `fontsize`) VALUES
-(1, 'nikos25', 'nikos25', 'email@email1.com', 'Nikos Athanasiou', 'Male', 'This is my bio bla bla bla bla', '2610995924', '6999999998', 'ADMIN', 'https://i.ibb.co/jv0cjvP/4EaxKrr.jpg', 'Dark Theme', 'Greek', 14),
-(2, 'makisKar', 'makismakis', 'email@email2.com', 'Makis Karapialis', 'Male', 'This is my bio bla bla bla bla', '2610-995990', '6999999999', 'ADMIN', 'https://i.ibb.co/hBqnhFq/8ZOn5uw.jpg', 'Dark Theme', 'Greek', 14),
-(3, 'giannisPapathanasiou', 'giannisPapathanasiou', 'email@email3.com', 'Giannis Papathanasios', 'Male', '12312312312313123', '2610312111', '69999999991', 'USER', 'https://i.ibbsdasda.co/TbT8Q3r/meLd3mN.jpg', 'Dark Theme', 'English', 16),
-(4, 'MariaGiannak', 'MariaGiannak', 'email@email4.com', 'Maria Giannakopoulou', 'Female', 'This is my bio bla bla bla bla', '2610-995990', '6999999999', 'ADMIN', 'https://i.ibb.co/6DF0dTw/a60d685194a7fd984d08a595a0a99ae7.jpg', 'Dark Theme', 'Greek', 12),
-(5, 'asdasdas', '123', 'asdasda', 'asd', 'Male', 'asdasda', 'asdasd', 'eqwqew', 'ADMIN', 'https://i.ibb.co/6DF0dTw/a60d685194a7fd984d08a595a0a99ae7.jpg', 'Dark Theme', 'Greek', 12);
+(1, 'nikos25', 'nikos25', 'email@email1.com', 'Nikos Athanasiou', 'Male', 'This is my bio bla bla bla bla', '2610-995990', '6999999999', 'ADMIN', 'https://i.ibb.co/jv0cjvP/4EaxKrr.jpg', 'Dark Theme', 'English', 16),
+(2, 'makisKar', 'makismakis', 'email@email2.com', 'Makis Karapialis', 'Male', 'This is my bio bla bla bla bla', '2610-995990', '6999999999', 'ADMIN', 'https://i.ibb.co/hBqnhFq/8ZOn5uw.jpg', '', '', 0),
+(3, 'giannisPapathanasiou', 'giannisPapathanasiou', 'email@email3.com', 'Giannis Papathanasiou', 'Male', 'This is my bio bla bla bla bla', '2610-995990', '6999999999', 'USER', 'https://i.ibb.co/TbT8Q3r/meLd3mN.jpg', '', '', 0),
+(4, 'MariaGiannak', 'MariaGiannak', 'email@email4.com', 'Maria Giannakopoulou', 'Female', 'This is my bio bla bla bla bla', '2610-995990', '6999999999', 'ADMIN', 'https://i.ibb.co/6DF0dTw/a60d685194a7fd984d08a595a0a99ae7.jpg', '', '', 0),
+(5, 'baggM1', 'baggm1', 'email@email5.com', 'Baggelis Antoniou1', 'Male', '1This is my bio bla bla bla bla', '6999999999', '6999999999', 'ADMIN', 'https://i.ibb.co/Tbmk05B/pexels-photo-220453.jpg', '', '', 0);
 
 --
 -- Indexes for dumped tables
@@ -137,7 +134,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `criminals`
@@ -149,7 +146,7 @@ ALTER TABLE `criminals`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
